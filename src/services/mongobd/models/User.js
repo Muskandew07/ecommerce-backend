e7b4531de5316d7931e9890af32ad7b37a32d53c
Muskandew07 <dewangan7muskan@gmail.com>
2022-04-26 23:23:21 +0530

    schema

--- src/services/mongobd/models/User.js
+++ src/services/mongobd/models/User.js
@@ -4,27 +4,23 @@ const UserSchema = new mongoose.Schema(
     firstName: {
       type: String,
       required: true,
-      minlength: 1,
+     
     },
     lastName: {
       type: String,
       required: true,
-      minlength: 1,
+      
     },
     email: {
       type: String,
       required: true,
-      unique: true,
+      
     },
     password: {
       type: String,
       required: true,
     },
-    role: {
-      type: Number,
-      default: 0,
-      // ! 0 is the non privileged user & 1 is the admin user
-    },
+   
     addresses: [
       {
         type: mongoose.Types.ObjectId,
